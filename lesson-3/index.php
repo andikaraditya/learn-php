f553f8727ff65fe6a7621da19abf4285d4641de9
Andika <andikaraditya1@gmail.com>
2024-01-06 12:48:28 +0700

    lesson 4

--- lesson-3/index.php
+++ lesson-3/index.php
@@ -1,17 +1,26 @@
 <?php
 
-echo "Hello World ";
+    $name = "Yoshi";
+    $age = 30;
+
+    define("NAME","Mario");
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1><?php echo "Hello Dom" ?></h1>
+    <h2><?php echo $name ?></h2>
+    <h2><?php echo $age?></h2>
+    <h2><?php echo NAME?></h2>
 </body>
+
 </html>
